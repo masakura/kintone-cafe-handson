ec93a2cd3dda4bf8d7cc24f63d65c8dae2cae352
Tomo Masakura <tomo.masakura@gmail.com>
2016-05-03 20:03:36 +0900

    kintone アプリからのデータを表示

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Client = require('node-rest-client').Client;
+const client = new Client();
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+
+  client.get('https://7nkse.cybozu.com/k/v1/records.json?app=19', {
+    headers: { 'X-Cybozu-API-Token': process.env['KINTONE_API_KEY'] }
+  }, (data) => {
+    const prefectures = data.records
+        .map(row => ({
+          code: row.Number.value,
+          name: row.Single_line_text.value
+        }));
+    prefectures.sort((a, b) => a.code - b.code);
+
+    res.render('index', { title: 'Express', prefectures});
+  });
 });
 
 module.exports = router;
